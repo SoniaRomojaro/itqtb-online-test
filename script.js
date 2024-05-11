618dcbe41827cb27fa23d14176d22eb03c104e0f
SoniaRomojaro <soniaromojaro@gmail.com>
2024-05-11 14:55:14 +0200

    test istqb sonia

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function bienvenida(){
     let nombrebienvenida = document.getElementById("e9texto").value;
     let mensaje = "Bienvenido/a a su examen: " + nombrebienvenida + ". Por favor, complete las preguntas. Dispone de 60 minutos.";
     imprimir(mensaje,"funcionesbienvenida");
+    let newColor5 = "blue";
+    funcionesbienvenida.style.color = newColor5;
 }
 
  
@@ -18,7 +20,18 @@ function mostrarrespuesta () {
     let respuestaSeleccionada = document.getElementById("pregunta1").value;
     let mensaje = "Su respuesta: " + respuestaSeleccionada;
     imprimir(mensaje,"hacontestado");
+    let newColor = "violet";
+    pregunta1.style.color = newColor;
+    let newColor3 = "blue";
+    hacontestado.style.color = newColor3;
 
-    let parrafo = document.getElementById("mostrarrespuesta");
+    let parrafo = document.getElementById("mostrarrespuesta1");
     parrafo.innerText = "La respuesta correcta es B: Pruebas de Regresión";
+    let newColor2 = "green";
+    mostrarrespuesta1.style.color = newColor2;
+
+
+
+
+
 }
